d6c7c4343dae873e3ba9a4e5b873e552c3ea4145
Seid Yesuf <seidyesuf750@gmail.com>
2026-05-16 14:21:12 +0300

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const { Telegraf } = require("telegraf");
+
 const app = express();
-const bot = require("./bot");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply("Bot working"));
+
+app.get("/", (req, res) => {
+  res.send("Running");
+});
 
 const PORT = process.env.PORT || 3000;
-bot.launch()
-app.get("/", (req, res) => {
-  res.send("Bot is running");
-});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
 
-console.log("Bot running...");
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
